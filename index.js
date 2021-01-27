c928c85f8a9e119ff44def347f90044ba4492827
kromate <akpananthony33@gmail.com>
2021-01-27 23:27:53 +0100

    added get episodes

--- index.js
+++ index.js
@@ -13,50 +13,89 @@ app.listen(port)
 app.get('/search', (request, response) => {
     console.log(request.query.name);
 
-    quest(`https://www.animeout.xyz/?s=${request.query.name}`, (error, _response, html) => {
+    quest(`https://gogoanime.so//search.html?keyword=${request.query.name}`, (error, _response, html) => {
     if (!error && _response.statusCode == 200) {
           const $ = cheerio.load(html);
           const searchArray = [];
-          const imageArray = [];
-
-
-    $('img.attachment-kleo-post-grid.size-kleo-post-grid.wp-post-image').each((i,el)=>{
-        const img = $(el).attr('src');
-        imageArray.push(img)
-
-    })
-
         
-          $('h3 > a').each((i,el) => {
-            const title = $(el).text()
-            const link = $(el).attr('href');
-            searchArray.push({name:title, link:link, image:imageArray[i]})
+          $('ul.items li').each((i,el) => {
+            const title = $(el).find('p.name a').text();
+            const link = $(el).find('p.name a').attr('href');
+            const img = $(el).find('.img img').attr('src');
+            const release = $(el).find('p.released').text();
+            searchArray.push({name:title, link:link, image:img , release:release})
           });
           response.send(searchArray) 
         }
       });
 });
 
-//get Episode for anime endpoint
-app.get('/episode', (request, response) => {
-    // get the episodes in the anime by parsing all links that are videos
+//GET Anime details
+app.get('/desc', (request, response) => {
     console.log(request.query.link);
 
-    quest(request.query.link, (error, _response, html) => {
+    quest(`https://gogoanime.so${request.query.link}`, (error, _response, html) => {
     if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const availableepisodes = [];
+          const infoArray = [];
+            const name= (request.query.link).split('/')[2]
+            const id = $('input#movie_id').attr('value')
+        // infoArray details
+        // [0] = type
+        // [1] = summary
+        // [2] = genre
+        // [3] = release
+        // [4] = status
+        // [5] = other name
+        
+          $('.anime_info_body p.type').each((i,el) => {
+            const det = $(el).text().split(':')[1]
+            infoArray.push({i:det})
+          })
+
+          $('ul#episode_page li').each((i,el) => {
+            const start = $(el).text().split('-')[0]
+            const end = $(el).text().split('-')[1]
+            availableepisodes.push({start:start, end:end})
+          })
+          
+         let searchArray=   {
+             id:id,
+            name:name,
+            type:infoArray[0].i,
+                summary:infoArray[1].i,
+                genre:infoArray[2].i,
+                release:infoArray[3].i,
+                status:infoArray[4].i,
+                otherNames:infoArray[5].i,
+                 episodes:availableepisodes}
+
+          response.send(searchArray) 
+        }
+      });
+});
+
+//get Episode for anime endpoint
+app.get('/episodes', (request, response) => {
+    // get the episodes in the anime by parsing all links that are videos ${}
+    console.log(request.query);
+    quest(`https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=${request.query.start}&ep_end=${request.query.end}&id=${request.query.id}&default_ep=0&alias=${request.query.name}`, (error, _response, html) => {
+        console.log(error);
+        if (!error && _response.statusCode == 200) {
           const $ = cheerio.load(html);
           const episodeArr = [];
           $('a').each((i,el) => {
-              if($(el).attr('href').includes('.mkv' || '.mp4')){
                 const title = $(el).text()
                 const link = $(el).attr('href');
                 episodeArr.push({name:title, link:link})
-              }
          
           });
           response.send(episodeArr) 
         }
       });
+
+
 });
 
 //get Description for an for anime endpoint
